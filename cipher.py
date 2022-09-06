21befecb8a2587dbb850f2e61e1248b2e53e0656
seanfaubus <seanfaubus@gmail.com>
2022-09-06 17:07:08 -0500

    created first function

--- cipher.py
+++ cipher.py
@@ -1,7 +1,15 @@
+from fileio import read_keyvalue
+# res = {test_keys[i]: test_values[i] for i in range(len(test_keys))}
 def make_dictionary(l1, l2):
     # Write your code here
-    pass
+    dict = {l1[i]: l2[i] for i in range(len(l1))}
+
+    return dict
 
 def cipher(message, d):
     # Write your code here
     pass
+
+l1 = read_keyvalue('ciphercode.txt')[0]
+l2 = read_keyvalue('ciphercode.txt')[1]
+print(make_dictionary(l1,l2))
--- fileio.py
+++ fileio.py
@@ -1,11 +1,37 @@
 def read_keyvalue(filename):
     # Write your code here
-    pass
+    # Getting file input
+    path = "input/"
+    with open(path+filename, 'r') as f:
+        keyLetters = []
+        valueLetters = []
+        text = f.readlines()
+        for i in text:
+            if i.startswith('K'):
+                keyLetters.append(i[3:])
+                keyLetters = [char for word in keyLetters for char in word]
+                # keyLetters.pop()
+                # keyLetters.append(' ')
+                for i in range(len(keyLetters)):
+                    if keyLetters[i] == '\n':
+                        keyLetters[i] = ' '
+            else:
+                valueLetters.append(i[3:])
+                valueLetters = [char for word in valueLetters for char in word]
+        return keyLetters, valueLetters
+
 
 def read_message(filename):
     # Write your code here
+    # Getting file input
+    path = "input/"
+    with open(path+filename, 'r') as f:
+        text = f.readlines()
     pass
 
 def write_ciphered_messages(ciphered_message, filename):
     # Write your code here
     pass
+
+
+print(read_keyvalue('ciphercode.txt'))
